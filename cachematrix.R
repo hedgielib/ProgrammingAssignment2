1a949d385c1e687a13337b9f17d8d745c83b4f5e
hedgielib <abigailgoben@gmail.com>
2014-07-27 18:02:30 -0500

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of these functions is to find and store the inverse of a given matrix
+##in order to reduce the number of computational intensive operations that occur.
 
-## Write a short comment describing this function
+##Function Name: makeCacheMatrix
+##Function Purpose: This function creates a data type capable of holding a matrix
+##and its inverse
+##Input: Matrix
+##Output: Cache Matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(initialmatrix = matrix()) {
+    matrixinverse <- NULL
+    set <- function(newmatrix) {
+        initialmatrix <<- newmatrix
+        matrixinverse <<- NULL
+    }
+    get <- function() initialmatrix
+    setinverse <- function(solve) matrixinverse <<- solve
+    getinverse <- function() matrixinverse
+    list(set = set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse) 
+    }
+    
 }
 
+## Name: cacheSolve
+## Purpose: Find an inverse of a matrix by first checking cache and
+## if null solving for the inverse
+## Input: Matrix that has been processed through makeCacheMatrix
+## Output: Matrix Inverse
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(cachedmatrix, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inverseofmatrix <- cachedmatrix$getinverse()
+    if(!is.null(inverseofmatrix)) {
+        message("getting cached data")
+        return(inverseofmatrix)
+    }
+    matrix <- x$get()
+    inverseofmatrix <- solve(currentmatrix, ...)
+    cachedmatrix$setinverse(inverseofmatrix)
+    inverseofmatrix
 }
